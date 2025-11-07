a4c1f34f4bd8625ee92a2cc698b4d3feaf13d382
eunhye <dmsgp2627@naver.com>
2025-11-08 02:16:31 +0900

    페이지 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AppLayout from "./layout/AppLayout";
 import Homepage from "./pages/Homepage/Homepage";
 import MoviePage from "./pages/Movies/MoviePage";
 import MovieDetailPage from "./pages/MoviesDetail/MovieDetailPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+// import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
           {/* /movies, /movies/:id, /movies/:id/recommendation, /movies/:id/reviews */}
           <Route path="movies">
             <Route index element={<MoviePage />} />
-            <Route path=":id" element={<MovieDetailPage />} />
+            {/* <Route path=":id" element={<MovieDetailPage />} /> */}
 
 
             <Route path=":id/recommendation" element={<div>추천 영화</div>} />
