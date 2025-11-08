f96e5929927cfd12f392c5f3250592977fd25e81
eunhye <dmsgp2627@naver.com>
2025-11-09 08:41:06 +0900

    경로 오류 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,21 @@
 import axios from "axios"
-import.meta.env.VITE_KEY
+
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-console.log("api key",API_KEY ? "잘됨":"안됨:",API_KEY);
+console.log("api key",API_KEY ? "잘됨":"안됨");
 // axios ('https// ') 치기 귀찮 앞 주소를 기본값으로 지정하자
 
 //utils 웹페이지 도구같은 함수들을 넣어놓기 좋다
 // 인스턴스를 클릭하면 설정값으로 인스턴스를 생성할 수 있다
 
 const api = axios.create({
-    baseURL:"https://api.themoviedb.org/3",
-    headers:{
-        Accept:'application/json',
-        Authorization:`Bearer ${API_KEY}`
-    }
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
 });
+
 console.log("Axios",api.defaults);
 
 // 요청 인터셉터 추가하기
